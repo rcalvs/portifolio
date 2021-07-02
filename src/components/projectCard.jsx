0de2b0147b0c12f8a01b2d98e1e86b70865f610a
rcalvs <rafaelcalvette@gmail.com>
2021-07-02 16:56:08 -0300

    CSS basico - Cards

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -1,4 +1,5 @@
 import data from '../data/data.json'
+import '../App.css';
 
 // const teste1 = Object.keys(data); // Retorna as Chaves
 // const teste2 = Object.values(data); // Retorna os Valores em Array
@@ -8,10 +9,14 @@ function CardRender(){
   return (
     <div>
       <h3>Um Card de Renderização Prévia de Projeto</h3>
-
+    <div className="MasterCard">
       {Object.values(data).map((element) => (
-        <div>
-          <p>{element.name}</p>
+        <div className="Card">
+          <h3>{element.name}</h3>
+          <p>{element.status}</p>
+          <p>{element.data}</p>
+          <button>Conheça o Projeto</button>
+
         </div>
       ))
       
@@ -19,6 +24,10 @@ function CardRender(){
       {console.log(Object.values(data))}
 
     </div>
+
+    </div>
+    
+
   )
 } 
 
