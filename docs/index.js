f0b153c4b5b05b248dffe9875fdca52d7267a806
RCalvs <72619953+rcalvs@users.noreply.github.com>
2021-08-03 18:05:32 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ReactDOM.render>
     <App />
-  </React.StrictMode>,
+  </ReactDOM.render>,
   document.getElementById('root')
 );
 
