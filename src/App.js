5fe5005f23890b4e0f3518410229f4df068810bf
rcalvs <rafaelcalvette@gmail.com>
2021-09-10 15:55:30 -0300

    finished Responsive Desing

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ import HomeToDo from './pages/ToDoProject/HomeToDo';
 function App() {
   return (
       <Switch>
-        {/* <Route exact path="/" component={ Home }/>
-        <Route path="/portifolio" component={ Home }/> */}
-        <Route path="/Portifolio" component={ Home }/>
+        <Route exact path="/" component={ Home }/>
+        {/* <Route path="/portifolio" component={ Home }/> */}
+        {/* <Route path="/Portifolio" component={ Home }/> */}
 
         <Route exact path="/portifolio/todo" component={ HomeToDo }/>
         <Route exact path="/todo" component={ HomeToDo }/>
--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -1,7 +1,7 @@
 import data from '../data/data.json'
 import '../App.css';
 import { useHistory } from "react-router-dom";
-import {Link } from "react-router-dom"
+// import {Link } from "react-router-dom"
 // const teste1 = Object.keys(data); // Retorna as Chaves
 // const teste2 = Object.values(data); // Retorna os Valores em Array
 // const teste3 = Object.entries(data); // Retorna Array com Chave + Valor em Array
@@ -17,23 +17,28 @@ function Projetos(){
   };
 
   return (
-    <div className="font-mono w-4/5 m-auto">
+    <div className="font-mono w-full m-auto">
       <h1 className="dark:text-blue-200">Projetos Elaborados</h1>
       <br />
 
     <div className="flex flex-wrap flex-row justify-center items-center">
       {Object.values(data).map((element) => (
-        <div className="relative border rounded-lg border-gray-300 shadow-md m-2 w-48 h-48 bg-gray-100 hover:shadow-xl dark:bg-gray-400">
+        <div className="text-xs relative border rounded-lg border-gray-300 shadow-md m-1 w-36 h-24 bg-gray-100 
+          md:relative md:m-2 md:w-48 md:h-48 md:text-lg
+          hover:shadow-xl hover:border-gray-700
+          dark:bg-gray-300 dark:border-gray-700 dark:hover:border-gray-100">
           <h3 className="p-3">
             {element.name}
           </h3>
-          <p className="italic text-base p-8 pt-2 pb-2">
+          <p className="italic p-auto m-auto md:text-base md:p-8 md:pt-2 md:pb-2">
             {element.status}
           </p>
           <button
             name={element.code}
             onClick={handleClick}
-            className="absolute bottom-5 left-0 rounded-r-lg mt-6 p-1 bg-blue-500 w-30 text-white font-bold hover:bg-blue-300"
+            className="absolute rounded-r-lg p-1 w-30 bottom-0 left-0
+              md:mt-6 md:p-1 md:w-30 md:bottom-5 md:left-0
+              bg-blue-500 text-white hover:bg-blue-400"
           >
             Conheça o Projeto
           </button>
